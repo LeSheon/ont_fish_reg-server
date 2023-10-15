a55f76d9a3c0ec6b24f8b1b33eacdda3758ad26f
Sheon <dlelfo7@gmail.com>
2023-10-15 12:55:03 -0400

    Fix seed to seed 20files

--- seeds/02_zone.js
+++ seeds/02_zone.js
@@ -1,8 +1,12 @@
 const { addZoneZero, getValidTimestamp } = require("../utils/db_helpers");
 const zoneData = [require("./seed_data/zone_01.json"), require("./seed_data/zone_02.json")
 ,require("./seed_data/zone_03.json"), require("./seed_data/zone_04.json"), require("./seed_data/zone_05.json")
-,require("./seed_data/zone_06.json")];
-
+, require("./seed_data/zone_06.json"), require("./seed_data/zone_07.json"), require("./seed_data/zone_08.json")
+, require("./seed_data/zone_09.json"), require("./seed_data/zone_10.json"), require("./seed_data/zone_11.json")
+, require("./seed_data/zone_12.json"), require("./seed_data/zone_13.json"), require("./seed_data/zone_14.json")
+, require("./seed_data/zone_15.json"), require("./seed_data/zone_16.json"), require("./seed_data/zone_17.json")
+, require("./seed_data/zone_18.json"), require("./seed_data/zone_19.json"), require("./seed_data/zone_20.json")
+];
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
 
@@ -10,7 +14,7 @@ exports.seed = async function(knex) {
     knex(`zone_${addZoneZero(i)}`).del();
   }
 
-  for(let i=1;i<7;i++){
+  for(let i=1;i<21;i++){
 
     let currentZoneData = zoneData[i-1];
     for(let l=0;l<currentZoneData.length;l++){
